4a5fdb2cd5338fde424262a6cb43071f77966f56
Deepak Sharma <sharma.jnu@gmail.com>
2017-04-16 17:39:06 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public snackBar: MdSnackBar) {
+
+  }
+
   title = 'app works!';
+
+  states = ['Delhi', 'Karnataka', 'Tamilnadu'];
+
+  openSnackBar() {
+    // this.snackBar.openFromComponent(AppComponent, {
+    //   duration: 1000
+    // });
+
+    this.snackBar.open("Hello how are you",undefined, {duration: 2000});
+  }
 }
